2d694244fcb16253027ee74cb404c93fc6e587c4
Megha katigar <98883965+MeghaKatigar@users.noreply.github.com>
2022-02-12 01:27:46 +0530

    Add files via upload

---
+++ 2_Implementation/Inc/main.h
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+typedef struct{
+	char name[50];
+	int train_num;
+	int num_of_seats;
+}pd;
+
+struct login                           // before the first use of `l`.
+{
+    char fname[30];
+    char lname[30];
+    char username[30];
+    char password[20];
+};
+
+
+
+void Reservation(void);
+void viewdetails(void);
+void cancellation(void);
+void printticket(char name[],int,int,float);
+void specific_train(int);
+float total_amount(int,int);
+void login(void);
+void registration (void);
+
+bool val = false;
+bool glob= false;
+bool Rev= false;
+int T_No=0;
